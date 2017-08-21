965278757392a1077c195f52014e0b1692b91511
Andrew Patrick Bartson <cloudobserver@gmail.com>
2017-08-20 21:05:05 -0700

    Trip Forecast - begin complete rebuild, take no prisoners

---
+++ _js/calculations.js
@@ -0,0 +1,12 @@
+// calculations.js
+
+function calc() {
+	return false;/*
+    ia = parseInt(document.getElementById('a').value);
+	ib = parseInt(document.getElementById('b').value);
+	document.getElementById('sum').value = ia + ib;
+	document.getElementById('product').value = ia * ib;
+	document.getElementById('diff').value = Math.max(ia, ib) - Math.min(ia, ib);
+	document.getElementById('quotient').value = Math.floor(ia / ib);
+	document.getElementById('remainder').value = ia % ib;*/	
+}
